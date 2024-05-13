4679d0fe1ba0084aca02adcfaa425584c9e7dd65
Fernando Perez <ferpega03@gmail.com>
2024-05-12 22:31:44 -0600

    nuevo routing

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -7,28 +7,20 @@ import MainPage from './MainPage';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isSignUp, setIsSignUp] = useState(true);
 
   const handleAuthentication = () => {
     setIsAuthenticated(true);
-  };
-
-  const togglePage = () => {
-    setIsSignUp(!isSignUp);
   };
 
   return (
     <Router>
       <Routes>
         {isAuthenticated ? ( //usuario ya autentificado
-          <Routes>
             <Route path="/" element={<MainPage />} />
-          </Routes>
         ) : (
           <>
-            <Route path="/" element={isSignUp ? <SignUp onAuthentication={handleAuthentication} togglePage={togglePage} /> : <Login onAuthentication={handleAuthentication} togglePage={togglePage} />} />
-            <Route path="/signup" element={<SignUp onAuthentication={handleAuthentication} togglePage={togglePage} />} />
-            <Route path="/login" element={<Login onAuthentication={handleAuthentication} togglePage={togglePage} />} />
+            <Route path="/" element={<Login onAuthentication={handleAuthentication}/> } />
+            <Route path="/signup" element={<SignUp onAuthentication={handleAuthentication} />} />
           </>
         )}
       </Routes>
