f7e92d7ee5736afd2034bf155227ec3eda7c15d6
Fernando Perez <ferpega03@gmail.com>
2024-05-12 23:36:23 -0600

    background chafa

--- client/my-app/src/MainPage.jsx
+++ client/my-app/src/MainPage.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 
 const MainPage = () => {
   return (
-    <div>
+    <div style={{ 
+      backgroundImage: `url(/background@2x.png)`, // directly reference the image in the public folder
+      backgroundSize: 'cover', // cover ensures the image covers the whole div
+      height: '100vh', // sets the height of the div to the full viewport height
+      color: 'black' // sets the text color to white for better visibility
+    }}>
       <h1>Bienvenido a la página principal</h1>
       <p>¡Has iniciado sesión correctamente!</p>
       {/* Aquí puedes agregar cualquier contenido adicional que desees mostrar en la página principal */}
