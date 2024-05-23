9c7a014b25f008e687a6d54b38a2fd7b65b06424
Fernando Perez <ferpega03@gmail.com>
2024-05-23 10:13:06 -0600

    header sin h3

--- client/my-app/src/Components/Header.jsx
+++ client/my-app/src/Components/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = ({ userInfo }) => {
   return (
     <div className="header">
-      <h1>Welcome back! <h3>{userInfo.info.nombre}</h3> </h1>
+      <h1>Welcome back! <p>{userInfo.info.nombre}</p> </h1>
       <img src="/logo.png" alt="Logo" className="logo"/>
     </div>
   );
