6b38aeed47825768e4b0d04c9819b89ddc2d7642
Fernando Perez <ferpega03@gmail.com>
2024-05-23 10:13:15 -0600

    navbar con logout

--- client/my-app/src/Components/NavBar.jsx
+++ client/my-app/src/Components/NavBar.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../AuthContext';
+import { useNavigate } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  }
+
+  const { logout } = useContext(AuthContext);
   return (
     <div className="navbar">
       <div className="nav-icon"><i className="fa fa-user"></i></div>  
       <div className="nav-icon"><i className="fa fa-home"></i></div>
       <div className="nav-icon"><i className="fa fa-calendar"></i></div>
       <div className="nav-icon"><i className="fa fa-tasks"></i></div>
-      <div className="nav-icon logout"><i className="fa fa-sign"></i></div>
+      <div className="nav-icon logout" onClick={handleLogout}><i className="fa fa-sign"></i></div>
     </div>
   );
 };
