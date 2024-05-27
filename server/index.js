61f269c030296d7ea253b1e3faf2b55d0c9fc20c
Fernando Perez <ferpega03@gmail.com>
2024-05-26 23:40:31 -0600

    cookie config2

--- server/index.js
+++ server/index.js
@@ -56,7 +56,8 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: { 
-      secure: true,  // Use secure cookies in production
+      secure: false,  // Use secure cookies in production
+      httpOnly: true,
     }
   })
 );
@@ -135,7 +136,7 @@ app.post("/login", async (req, res) => {
         const userType = checkResult.rows[0].tipousuario;
         req.session.userId = userId;
         req.session.userType = userType;
-        console.log(userId);
+        console.log(userId, userType);
         res.status(200).json({ message: "Login successful", authenticated: true });
       } else {
         res.status(401).json({ error: "Invalid credentials" });
